23ce71b00fd87d485c77aa10b18b37b6ce78aef8
mateuscavati <mateusmcs.profissional@outlook.com>
2026-06-04 16:49:11 -0300

    a

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function createWindow() {
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js'),
     },
-    icon: path.join(__dirname, 'frontend-apuama/public/icon.svg'),
+    icon: path.join(__dirname, 'frontend-apuama/public/Logo_APU.png'),
   });
 
   if (isDev) {
